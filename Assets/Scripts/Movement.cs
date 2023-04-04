83efcae9954598c5b5113c74329dedd808c97933
Pever3ll <cloakofpeverell@gmail.com>
2023-04-04 11:28:57 -0700

    Started Movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float horizontal;
+    private float vertical;
+    public Rigidbody2D rb;
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        //rb.AddForce(new Vector2(horizontal, 0) * Time.deltaTime * 100, ForceMode2D.Force);
+        rb.AddForce(new Vector2(1,0), ForceMode2D.Force);
+        //Debug.Log(Time.deltaTime);
     }
 }
