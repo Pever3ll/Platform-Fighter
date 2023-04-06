477448b1f159a9ef577adc5f5f7ae6667ad195f2
Pever3ll <cloakofpeverell@gmail.com>
2023-04-05 17:57:20 -0700

    Max vel and added floor material

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,6 +8,8 @@ public class Movement : MonoBehaviour
     public float horizontal;
     private float vertical;
     public Rigidbody2D rb;
+    public float accel;
+    public float maxSpeed;
 
     // Update is called once per frame
     void Update()
@@ -18,8 +20,25 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-        //rb.AddForce(new Vector2(horizontal, 0) * Time.deltaTime * 100, ForceMode2D.Force);
-        rb.AddForce(new Vector2(1,0), ForceMode2D.Force);
-        //Debug.Log(Time.deltaTime);
+        rb.AddForce(new Vector2(horizontal * accel,0), ForceMode2D.Force);
+        
+        if (Mathf.Abs(rb.velocity.x) >= maxSpeed)
+        {
+            Vector2 velocity = rb.velocity;
+            float direction = velocity.x / Mathf.Abs(velocity.x);
+            rb.velocity = new Vector2(direction * maxSpeed, velocity.y);
+            rb.AddForce(new Vector2(-direction * accel * Mathf.Abs(horizontal),0), ForceMode2D.Force);
+            /*
+            if (rb.velocity.x < 1)
+            {
+                rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
+                rb.AddForce(new Vector2(accel * Mathf.Abs(horizontal),0), ForceMode2D.Force);
+            }else if (rb.velocity.x > 1)
+            {
+                rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+                rb.AddForce(new Vector2(-accel * Mathf.Abs(horizontal),0), ForceMode2D.Force);
+            }
+            */
+        }
     }
 }
