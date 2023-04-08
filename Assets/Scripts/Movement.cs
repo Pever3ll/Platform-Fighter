c9508915d6338490254160718399e7967e43c556
Pever3ll <cloakofpeverell@gmail.com>
2023-04-07 17:08:54 -0700

    Smooth Movement + Friction

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,6 +10,9 @@ public class Movement : MonoBehaviour
     public Rigidbody2D rb;
     public float accel;
     public float maxSpeed;
+    private float tempMaxSpeed;
+    public float accelConst;
+    public float friction;
 
     // Update is called once per frame
     void Update()
@@ -21,16 +24,46 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
+        //Sets Max velocity as joystick dependent
+        if (horizontal != 0)
+        {
+            tempMaxSpeed = maxSpeed * Mathf.Abs(horizontal);
+        }
+        else
+        {
+            tempMaxSpeed = maxSpeed;
+        }
+        //Debug.Log(tempMaxSpeed);
+        Vector2 velocity = rb.velocity;
+        float direction = velocity.x / Mathf.Abs(velocity.x);
+        
         //Horizontal Movement
-        rb.AddForce(new Vector2(horizontal * accel,0), ForceMode2D.Force);
+        rb.AddForce(new Vector2(horizontal * accel * accelConst,0), ForceMode2D.Force);
         //Checks for horizontal speed
-        if (Mathf.Abs(rb.velocity.x) >= maxSpeed)
+        if (Mathf.Abs(rb.velocity.x) >= tempMaxSpeed)
         {
             //Sets to terminal velocity
-            Vector2 velocity = rb.velocity;
-            float direction = velocity.x / Mathf.Abs(velocity.x);
-            rb.velocity = new Vector2(direction * maxSpeed, velocity.y);
-            rb.AddForce(new Vector2(-direction * accel * Mathf.Abs(horizontal),0), ForceMode2D.Force);
+            rb.velocity = new Vector2(direction * tempMaxSpeed, velocity.y);
+            rb.AddForce(new Vector2(-direction * accel * accelConst * Mathf.Abs(horizontal),0), ForceMode2D.Force);
+        }
+        
+        //Friction
+        if (horizontal == 0 && velocity.x != 0)
+        {
+            float fricForce = -direction * accelConst * friction;
+            float velChange = fricForce * Time.deltaTime;
+            //Debug.Log("Fricforce: " + fricForce);
+            //Debug.Log("velChange: " + velChange);
+            //Debug.Log("velocity: " + velocity.x);
+            if ((velocity.x + velChange) * direction <= 0)
+            {
+                rb.velocity = new Vector2(0, velocity.y);
+            }
+            else
+            {
+                rb.AddForce(new Vector2(fricForce, 0), ForceMode2D.Force);
+            }
+            //Debug.Log("Future Vel: " + (velocity.x + velChange) * direction);
         }
     }
 }
